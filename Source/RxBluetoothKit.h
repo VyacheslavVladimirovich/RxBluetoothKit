f0e987f61080c2192393ac165f6fe9576fb6714f
VyacheslavKazakov <vyacheslav@blackbricks.io>
2021-02-17 12:23:50 +0600

    - update podspec

--- Source/RxBluetoothKit.h
+++ Source/RxBluetoothKit.h
@@ -8,5 +8,3 @@ FOUNDATION_EXPORT const unsigned char RxBluetoothKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using
 // statements like #import <RxBluetoothKit/PublicHeader.h>
-
-
